6bbc0a81fcac20a6a5eb2ab8e0618cda62b15948
alyza <alyza@XHUMANITY>
2023-04-09 21:01:10 +0800

    Add project files.

---
+++ MenuForm.vb
@@ -0,0 +1,16 @@
+﻿Public Class MenuForm
+    Private Sub ButtonAddrecord_Click(sender As Object, e As EventArgs) Handles ButtonAddrecord.Click
+        Choices.Show()
+        Hide()
+    End Sub
+
+    Private Sub ButtonLoad_Click(sender As Object, e As EventArgs) Handles ButtonLoad.Click
+        Load_data.Show()
+        Hide()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Login.Show()
+        Hide()
+    End Sub
+End Class
---
+++ contact_details.Designer.vb
@@ -0,0 +1,177 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class contact_details
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextID = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextCp = New System.Windows.Forms.TextBox()
+        Me.TextPnum = New System.Windows.Forms.TextBox()
+        Me.ButtonAdd = New System.Windows.Forms.Button()
+        Me.ButtonCancel = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ButtonHome = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Century Gothic", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(147, 43)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(190, 28)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "Contact Details"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(81, 105)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(21, 17)
+        Me.Label1.TabIndex = 13
+        Me.Label1.Text = "ID"
+        '
+        'TextID
+        '
+        Me.TextID.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextID.Location = New System.Drawing.Point(185, 99)
+        Me.TextID.Name = "TextID"
+        Me.TextID.Size = New System.Drawing.Size(192, 23)
+        Me.TextID.TabIndex = 14
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(81, 176)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(103, 17)
+        Me.Label2.TabIndex = 15
+        Me.Label2.Text = "Phone number"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(69, 143)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(110, 17)
+        Me.Label3.TabIndex = 16
+        Me.Label3.Text = "Contact person"
+        '
+        'TextCp
+        '
+        Me.TextCp.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextCp.Location = New System.Drawing.Point(185, 137)
+        Me.TextCp.Name = "TextCp"
+        Me.TextCp.Size = New System.Drawing.Size(192, 23)
+        Me.TextCp.TabIndex = 17
+        '
+        'TextPnum
+        '
+        Me.TextPnum.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextPnum.Location = New System.Drawing.Point(185, 170)
+        Me.TextPnum.Name = "TextPnum"
+        Me.TextPnum.Size = New System.Drawing.Size(192, 23)
+        Me.TextPnum.TabIndex = 18
+        '
+        'ButtonAdd
+        '
+        Me.ButtonAdd.Font = New System.Drawing.Font("Century Gothic", 10.75!, System.Drawing.FontStyle.Bold)
+        Me.ButtonAdd.Location = New System.Drawing.Point(84, 209)
+        Me.ButtonAdd.Name = "ButtonAdd"
+        Me.ButtonAdd.Size = New System.Drawing.Size(293, 29)
+        Me.ButtonAdd.TabIndex = 19
+        Me.ButtonAdd.Text = "Add"
+        Me.ButtonAdd.UseVisualStyleBackColor = True
+        '
+        'ButtonCancel
+        '
+        Me.ButtonCancel.Font = New System.Drawing.Font("Century Gothic", 10.75!, System.Drawing.FontStyle.Bold)
+        Me.ButtonCancel.Location = New System.Drawing.Point(84, 253)
+        Me.ButtonCancel.Name = "ButtonCancel"
+        Me.ButtonCancel.Size = New System.Drawing.Size(293, 30)
+        Me.ButtonCancel.TabIndex = 20
+        Me.ButtonCancel.Text = "Cancel"
+        Me.ButtonCancel.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Century Gothic", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(379, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 21
+        Me.Button1.Text = "Back"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ButtonHome
+        '
+        Me.ButtonHome.Font = New System.Drawing.Font("Century Gothic", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonHome.Location = New System.Drawing.Point(27, 12)
+        Me.ButtonHome.Name = "ButtonHome"
+        Me.ButtonHome.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonHome.TabIndex = 22
+        Me.ButtonHome.Text = "Menu"
+        Me.ButtonHome.UseVisualStyleBackColor = True
+        '
+        'contact_details
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(466, 308)
+        Me.Controls.Add(Me.ButtonHome)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ButtonCancel)
+        Me.Controls.Add(Me.ButtonAdd)
+        Me.Controls.Add(Me.TextPnum)
+        Me.Controls.Add(Me.TextCp)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextID)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label5)
+        Me.Name = "contact_details"
+        Me.Text = "contact_details"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextID As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextCp As TextBox
+    Friend WithEvents TextPnum As TextBox
+    Friend WithEvents ButtonAdd As Button
+    Friend WithEvents ButtonCancel As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ButtonHome As Button
+End Class
